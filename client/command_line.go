135d43beaf03a14f8145a40e7f0998072b31d591
ivanpoltavskiy <ivan.poltavskyi@teamdev.com>
2024-06-14 11:28:54 +0300

    Update the method layout in the command line client.

--- client/command_line.go
+++ client/command_line.go
@@ -70,53 +70,6 @@ func (cli *CommandLineClient) FetchArticles() ([]article.Article, error) {
 	return articles, nil
 }
 
-// sortedByDate sorts news by ASC or DESC.
-func (cli *CommandLineClient) sortedByDate(articles []article.Article) {
-
-	if strings.ToLower(cli.sortBy) == "asc" {
-		sort.Slice(articles, func(i, j int) bool {
-			return articles[i].Date.Before(articles[j].Date)
-		})
-	} else if strings.ToLower(cli.sortBy) == "desc" {
-		sort.Slice(articles, func(i, j int) bool {
-			return articles[i].Date.After(articles[j].Date)
-		})
-	}
-}
-
-// fetchParameters extracts and validates command line parameters,
-// including sources and filters, and returns them for use in article fetching.
-func fetchParameters(cli *CommandLineClient) ([]filter.ArticleFilter, []string) {
-	sourceNames := strings.Split(cli.sources, ",")
-	var filters []filter.ArticleFilter
-
-	filters = fetchKeywords(cli, filters)
-	filters = fetchDateFilters(cli, filters)
-	uniqueSources := validator.CheckUnique(sourceNames)
-	return filters, uniqueSources
-}
-
-// fetchKeywords extracts keywords from command line arguments and adds them to the filters.
-func fetchKeywords(cli *CommandLineClient, filters []filter.ArticleFilter) []filter.ArticleFilter {
-	if cli.keywords != "" {
-		keywords := strings.Split(cli.keywords, ",")
-		uniqueKeywords := validator.CheckUnique(keywords)
-		filtersForTemplate = append(filtersForTemplate, "Keywords: "+strings.Join(uniqueKeywords, ", "))
-		filters = append(filters, filter.ByKeyword{Keywords: uniqueKeywords})
-	}
-	return filters
-}
-
-// fetchDateFilters extracts date filters from command line arguments and adds them to the filters.
-func fetchDateFilters(cli *CommandLineClient, filters []filter.ArticleFilter) []filter.ArticleFilter {
-	isValid, startDate, endDate := validator.CheckData(cli.startDateStr, cli.endDateStr)
-	if isValid {
-		filtersForTemplate = append(filtersForTemplate, fmt.Sprintf("Date filter - %s to %s", cli.startDateStr, cli.endDateStr))
-		filters = append(filters, filter.ByDate{StartDate: startDate, EndDate: endDate})
-	}
-	return filters
-}
-
 // Print prints the provided articles using a template.
 func (cli *CommandLineClient) Print(articles []article.Article) {
 	funcMap := template.FuncMap{
@@ -126,7 +79,7 @@ func (cli *CommandLineClient) Print(articles []article.Article) {
 			} else {
 				for _, keyword := range strings.Split(keywords, ",") {
 					re := regexp.MustCompile(`(?i)` + regexp.QuoteMeta(keyword))
-					text = re.ReplaceAllString(text, "**"+keyword+"**")
+					text = re.ReplaceAllString(text, "//"+keyword+"//")
 				}
 				return text
 			}
@@ -179,4 +132,51 @@ func (cli *CommandLineClient) Print(articles []article.Article) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+// sortedByDate sorts news by ASC or DESC.
+func (cli *CommandLineClient) sortedByDate(articles []article.Article) {
+
+	if strings.ToLower(cli.sortBy) == "asc" {
+		sort.Slice(articles, func(i, j int) bool {
+			return articles[i].Date.Before(articles[j].Date)
+		})
+	} else if strings.ToLower(cli.sortBy) == "desc" {
+		sort.Slice(articles, func(i, j int) bool {
+			return articles[i].Date.After(articles[j].Date)
+		})
+	}
+}
+
+// fetchParameters extracts and validates command line parameters,
+// including sources and filters, and returns them for use in article fetching.
+func fetchParameters(cli *CommandLineClient) ([]filter.ArticleFilter, []string) {
+	sourceNames := strings.Split(cli.sources, ",")
+	var filters []filter.ArticleFilter
+
+	filters = fetchKeywords(cli, filters)
+	filters = fetchDateFilters(cli, filters)
+	uniqueSources := validator.CheckUnique(sourceNames)
+	return filters, uniqueSources
+}
+
+// fetchKeywords extracts keywords from command line arguments and adds them to the filters.
+func fetchKeywords(cli *CommandLineClient, filters []filter.ArticleFilter) []filter.ArticleFilter {
+	if cli.keywords != "" {
+		keywords := strings.Split(cli.keywords, ",")
+		uniqueKeywords := validator.CheckUnique(keywords)
+		filtersForTemplate = append(filtersForTemplate, "Keywords: "+strings.Join(uniqueKeywords, ", "))
+		filters = append(filters, filter.ByKeyword{Keywords: uniqueKeywords})
+	}
+	return filters
+}
+
+// fetchDateFilters extracts date filters from command line arguments and adds them to the filters.
+func fetchDateFilters(cli *CommandLineClient, filters []filter.ArticleFilter) []filter.ArticleFilter {
+	isValid, startDate, endDate := validator.CheckData(cli.startDateStr, cli.endDateStr)
+	if isValid {
+		filtersForTemplate = append(filtersForTemplate, fmt.Sprintf("Date filter - %s to %s", cli.startDateStr, cli.endDateStr))
+		filters = append(filters, filter.ByDate{StartDate: startDate, EndDate: endDate})
+	}
+	return filters
 }
