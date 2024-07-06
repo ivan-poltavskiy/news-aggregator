e6ae5044413cd53d69b95da316442cb5cd02f155
ivanpoltavskiy <ivan.poltavskyi@teamdev.com>
2024-07-06 13:20:46 +0300

    Add https.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,13 +8,15 @@ import (
 )
 
 func main() {
+	certFile := "certificates/server.crt"
+	keyFile := "certificates/server.key"
 
 	http.HandleFunc("GET /articles", handlers.FetchArticleHandler)
 	http.HandleFunc("POST /sources", handlers.AddSourceHandler)
 	http.HandleFunc("DELETE /sources", handlers.DeleteSourceByNameHandler)
 	logrus.Info("Starting server on " + constant.PORT)
 
-	err := http.ListenAndServe(constant.PORT, nil)
+	err := http.ListenAndServeTLS(constant.PORT, certFile, keyFile, nil)
 	if err != nil {
 		logrus.Fatalf("Could not start server: %s\n", err.Error())
 	}
--- constant/const.go
+++ constant/const.go
@@ -4,6 +4,6 @@ const DateOutputLayout = "2006-01-02"
 
 var PathToStorage = "./storage/sources-storage.json"
 
-const PORT = ":8080"
+const PORT = ":443"
 
 var PathToResources = "resources"
