964f21c6e1292e899eceb59752cd07b36ccc9c07
ivan <ivan.poltavskyi@teamdev.com>
2024-07-23 02:56:16 +0300

    Add tests for source json storage.

--- storage/source/json-storage_test.go
+++ storage/source/json-storage_test.go
@@ -63,3 +63,170 @@ func TestIsSourceExists(t *testing.T) {
 		})
 	}
 }
+
+func TestSaveSource(t *testing.T) {
+	sources := []source.Source{
+		{Name: "Source1"},
+	}
+	_, err := json.Marshal(sources)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	newSource := source.Source{Name: "Source2"}
+
+	tests := []struct {
+		name      string
+		existing  []source.Source
+		newSource source.Source
+		expected  []source.Source
+		expectErr bool
+	}{
+		{
+			name:      "add new source",
+			existing:  sources,
+			newSource: newSource,
+			expected:  append(sources, newSource),
+			expectErr: false,
+		},
+		{
+			name:      "source already exists",
+			existing:  append(sources, newSource),
+			newSource: newSource,
+			expected:  append(sources, newSource),
+			expectErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			existingData, err := json.Marshal(tt.existing)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			filePath := setupTempFile(t, existingData)
+			defer teardownTempFile(filePath)
+
+			storage := &jsonSourceStorage{pathToStorage: source.PathToFile(filePath)}
+			err = storage.SaveSource(tt.newSource)
+			if (err != nil) != tt.expectErr {
+				t.Fatalf("SaveSource() error = %v, wantErr %v", err, tt.expectErr)
+			}
+
+			if err != nil {
+				return
+			}
+
+			sources, err := storage.GetSources()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			assert.ElementsMatch(t, tt.expected, sources)
+		})
+	}
+}
+
+func TestDeleteSourceByName(t *testing.T) {
+	sources := []source.Source{
+		{Name: "Source1"},
+		{Name: "Source2"},
+	}
+	data, err := json.Marshal(sources)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tests := []struct {
+		name      string
+		inputName string
+		expected  []source.Source
+		expectErr bool
+	}{
+		{
+			name:      "delete existing source",
+			inputName: "Source1",
+			expected:  []source.Source{{Name: "Source2"}},
+			expectErr: false,
+		},
+		{
+			name:      "delete non-existing source",
+			inputName: "Source3",
+			expected:  sources,
+			expectErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			filePath := setupTempFile(t, data)
+			defer teardownTempFile(filePath)
+
+			storage := &jsonSourceStorage{pathToStorage: source.PathToFile(filePath)}
+			err = storage.DeleteSourceByName(tt.inputName)
+			if (err != nil) != tt.expectErr {
+				t.Fatalf("DeleteSourceByName() error = %v, wantErr %v", err, tt.expectErr)
+			}
+
+			if err != nil {
+				return
+			}
+
+			sources, err := storage.GetSources()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			assert.ElementsMatch(t, tt.expected, sources)
+		})
+	}
+}
+
+func TestGetSourceByName(t *testing.T) {
+	sources := []source.Source{
+		{Name: "Source1"},
+		{Name: "Source2"},
+	}
+	data, err := json.Marshal(sources)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tests := []struct {
+		name      string
+		inputName source.Name
+		expected  source.Source
+		expectErr bool
+	}{
+		{
+			name:      "get existing source",
+			inputName: "Source1",
+			expected:  sources[0],
+			expectErr: false,
+		},
+		{
+			name:      "get non-existing source",
+			inputName: "Source3",
+			expected:  source.Source{},
+			expectErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			filePath := setupTempFile(t, data)
+			defer teardownTempFile(filePath)
+
+			storage := &jsonSourceStorage{pathToStorage: source.PathToFile(filePath)}
+			source, err := storage.GetSourceByName(tt.inputName)
+			if (err != nil) != tt.expectErr {
+				t.Fatalf("GetSourceByName() error = %v, wantErr %v", err, tt.expectErr)
+			}
+
+			if err == nil {
+				assert.Equal(t, tt.expected, source)
+			}
+		})
+	}
+}
