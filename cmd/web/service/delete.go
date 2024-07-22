3946fccb731bf802db1b44dde8ea0b877adf8f7e
ivan <ivan.poltavskyi@teamdev.com>
2024-07-22 12:13:31 +0300

    Add doc for the method in delete service.

--- cmd/web/service/delete.go
+++ cmd/web/service/delete.go
@@ -5,6 +5,7 @@ import (
 	"news-aggregator/storage"
 )
 
+// DeleteSourceByName removes the source from storage by name.
 func DeleteSourceByName(name string, sourceStorage storage.Storage) error {
 	err := sourceStorage.DeleteSourceByName(name)
 	if err != nil {
