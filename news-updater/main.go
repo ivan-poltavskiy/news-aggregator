f3011ea38a185dc6c13b5f1ccdf2e2f02b977482
ivan <ivan.poltavskyi@teamdev.com>
2024-09-06 11:37:12 +0300

    Update err handling in main

--- news-updater/main.go
+++ news-updater/main.go
@@ -11,19 +11,19 @@ import (
 )
 
 func main() {
-	logrus.Info("res: " + constant.PathToResources)
-	logrus.Info("str: " + constant.PathToStorage)
-	newsJsonStorage, err := newsStorage.NewJsonStorage(source.PathToFile(constant.PathToResources))
-	if err != nil {
-		logrus.Fatal(err)
-	}
-	sourceJsonStorage, err := sourceStorage.NewJsonStorage(source.PathToFile(constant.PathToStorage))
-	if err != nil {
-		logrus.Fatal(err)
+	logrus.Info("Path to resources: " + constant.PathToResources)
+	logrus.Info("Path to storage: " + constant.PathToStorage)
+
+	newsJsonStorage, newsStorageErr := newsStorage.NewJsonStorage(source.PathToFile(constant.PathToResources))
+	sourceJsonStorage, sourceStorageErr := sourceStorage.NewJsonStorage(source.PathToFile(constant.PathToStorage))
+
+	if sourceStorageErr == nil && newsStorageErr == nil {
+		resourcesStorage := storage.NewStorage(newsJsonStorage, sourceJsonStorage)
+
+		service := updater.Service{Storage: resourcesStorage}
+		service.UpdateNews()
+	} else {
+		logrus.Fatal(sourceStorageErr, newsStorageErr)
 	}
 
-	resourcesStorage := storage.NewStorage(newsJsonStorage, sourceJsonStorage)
-
-	service := updater.Service{Storage: resourcesStorage}
-	service.UpdateNews()
 }
