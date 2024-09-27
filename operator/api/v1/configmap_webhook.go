4b048e72490985d608b2bdcbb6b39c7dd661e093
ivan <ivan.poltavskyi@teamdev.com>
2024-09-27 14:42:41 +0300

    Separate getting feeds from context to the method.

--- operator/api/v1/configmap_webhook.go
+++ operator/api/v1/configmap_webhook.go
@@ -3,9 +3,6 @@ package v1
 import (
 	"context"
 	"fmt"
-	"strings"
-	"time"
-
 	"github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -13,6 +10,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+	"strings"
 )
 
 // +kubebuilder:webhook:path=/validate--v1-configmap,mutating=false,failurePolicy=fail,sideEffects=None,groups="",resources=configmaps,verbs=create;update,versions=v1,name=vconfigmap.kb.io,admissionReviewVersions=v1
@@ -44,15 +42,20 @@ func (v *ConfigMapValidator) ValidateCreate(ctx context.Context, obj runtime.Obj
 
 	for key, value := range configMap.Data {
 		if value == "" {
-			errorsList = append(errorsList, field.Required(specPath.Child(key), "data cannot be empty"))
+			errorsList = append(errorsList, field.Required(specPath.Child(key), "feed group defined but no feeds mapped to it"))
 		}
+	}
+
+	existingFeeds, err := v.getFeedsFromContext(ctx, configMap)
+
+	if err != nil {
+		errorsList = append(errorsList, field.InternalError(specPath, fmt.Errorf("failed to retrieve feeds: %v", err)))
 	}
 
 	if len(errorsList) > 0 {
 		return nil, errorsList.ToAggregate()
 	}
-
-	return v.checkFeedsExist(ctx, configMap)
+	return v.checkFeedsExist(existingFeeds, configMap)
 }
 
 // ValidateUpdate implements validation logic for ConfigMap updates.
@@ -66,24 +69,7 @@ func (v *ConfigMapValidator) ValidateDelete(ctx context.Context, obj runtime.Obj
 }
 
 // checkFeedsExist checks whether the feeds specified in the ConfigMap exist in the namespace.
-func (v *ConfigMapValidator) checkFeedsExist(ctx context.Context, configMap *corev1.ConfigMap) (admission.Warnings, error) {
-	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
-	defer cancel()
-
-	logrus.Info("Validate feeds in the ConfigMap: ", configMap.Name)
-	logrus.Info("ConfigMap namespace: ", v.ConfigMapNamespace)
-
-	feedList := &FeedList{}
-	err := v.List(ctx, feedList, &client.ListOptions{Namespace: v.ConfigMapNamespace})
-	if err != nil {
-		return nil, fmt.Errorf("failed to list feeds: %v", err)
-	}
-	logrus.Info("Feeds in the namespace: ", feedList.Items)
-
-	existingFeeds := make(map[string]struct{})
-	for _, feed := range feedList.Items {
-		existingFeeds[feed.Spec.Name] = struct{}{}
-	}
+func (v *ConfigMapValidator) checkFeedsExist(existingFeeds map[string]struct{}, configMap *corev1.ConfigMap) (admission.Warnings, error) {
 
 	var errorsList field.ErrorList
 	specPath := field.NewPath("data")
@@ -104,4 +90,22 @@ func (v *ConfigMapValidator) checkFeedsExist(ctx context.Context, configMap *cor
 	}
 
 	return nil, nil
+}
+
+func (v *ConfigMapValidator) getFeedsFromContext(ctx context.Context, configMap *corev1.ConfigMap) (map[string]struct{}, error) {
+	logrus.Info("Validate feeds in the ConfigMap: ", configMap.Name)
+	logrus.Info("ConfigMap namespace: ", v.ConfigMapNamespace)
+
+	feedList := &FeedList{}
+	err := v.List(ctx, feedList, &client.ListOptions{Namespace: v.ConfigMapNamespace})
+	if err != nil {
+		return nil, fmt.Errorf("failed to list feeds: %v", err)
+	}
+	logrus.Info("Feeds in the namespace: ", feedList.Items)
+
+	existingFeeds := make(map[string]struct{})
+	for _, feed := range feedList.Items {
+		existingFeeds[feed.Spec.Name] = struct{}{}
+	}
+	return existingFeeds, nil
 }
