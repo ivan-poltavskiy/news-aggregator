47a18310718461994ab1416899b57384e0671fd8
ivanpoltavskiy <ivan.poltavskyi@teamdev.com>
2024-10-02 22:38:50 +0300

    feat: add parameters in the cdk.go config for more flexibility. Remove unused structure.

--- infra/cdk/cdk.go
+++ infra/cdk/cdk.go
@@ -16,20 +16,24 @@ type CdkStackProps struct {
 }
 
 var (
-	vpcCidr              = "10.0.0.0/16"
-	vpcName              = "ivan-cdk-vpc"
-	maxAzs               = 2
-	publicSubnetName     = "ivan-cdk-public-subnet-1"
-	privateSubnetName    = "ivan-cdk-private-subnet-1"
-	subnetCidrMask       = 20
-	accountID            = "406477933661"
-	region               = "eu-west-2"
-	routeTableName       = "ivan-cdk-rt"
-	nodeGroupMinSize     = 1
-	nodeGroupMaxSize     = 10
-	nodeGroupDesiredSize = 2
-	sshKeyName           = "ivan-kp"
-	diskSize             = 20
+	defaultVpcCidr              = "10.0.0.0/16"
+	defaultVpcName              = "ivan-cdk-vpc"
+	defaultMaxAzs               = 2
+	defaultPublicSubnetName     = "ivan-cdk-public-subnet-1"
+	defaultPrivateSubnetName    = "ivan-cdk-private-subnet-1"
+	defaultSubnetCidrMask       = 20
+	defaultAccountID            = "406477933661"
+	defaultRegion               = "eu-west-2"
+	defaultRouteTableName       = "ivan-cdk-rt"
+	defaultNodeGroupMinSize     = 1
+	defaultNodeGroupMaxSize     = 10
+	defaultNodeGroupDesiredSize = 2
+	defaultSshKeyName           = "ivan-kp"
+	defaultDiskSize             = 20
+	defaultCapacity             = 0
+	defaultDestinationCidrBlock = "0.0.0.0/0"
+	defaultK8SVersion           = "1.30"
+	defaultInstanceType         = "t2.medium"
 )
 
 // NewCdkIvanStack Returns the new stack with the cluster, nodegroup and additional resources
@@ -37,32 +41,32 @@ func NewCdkIvanStack(scope constructs.Construct, id string, props *CdkStackProps
 	stack := awscdk.NewStack(scope, &id, &props.StackProps)
 
 	//creates new vpc with parameters from vpcConfig
-	vpc := awsec2.NewVpc(stack, jsii.String(vpcName), &awsec2.VpcProps{
-		IpAddresses: awsec2.IpAddresses_Cidr(jsii.String(vpcCidr)),
-		MaxAzs:      jsii.Number(maxAzs),
+	vpc := awsec2.NewVpc(stack, jsii.String(defaultVpcName), &awsec2.VpcProps{
+		IpAddresses: awsec2.IpAddresses_Cidr(jsii.String(defaultVpcCidr)),
+		MaxAzs:      jsii.Number(defaultMaxAzs),
 		SubnetConfiguration: &[]*awsec2.SubnetConfiguration{
 			{
-				Name:                jsii.String(publicSubnetName),
+				Name:                jsii.String(defaultPublicSubnetName),
 				SubnetType:          awsec2.SubnetType_PUBLIC,
-				CidrMask:            jsii.Number(subnetCidrMask),
+				CidrMask:            jsii.Number(defaultSubnetCidrMask),
 				MapPublicIpOnLaunch: jsii.Bool(true),
 			},
 			{
-				Name:       jsii.String(privateSubnetName),
+				Name:       jsii.String(defaultPrivateSubnetName),
 				SubnetType: awsec2.SubnetType_PRIVATE_WITH_EGRESS,
-				CidrMask:   jsii.Number(subnetCidrMask),
+				CidrMask:   jsii.Number(defaultSubnetCidrMask),
 			},
 		},
-		VpcName: jsii.String(vpcName),
+		VpcName: jsii.String(defaultVpcName),
 	})
 
 	// creates new route table with id of the vpc
-	routeTable := awsec2.NewCfnRouteTable(stack, jsii.String(routeTableName), &awsec2.CfnRouteTableProps{
+	routeTable := awsec2.NewCfnRouteTable(stack, jsii.String(defaultRouteTableName), &awsec2.CfnRouteTableProps{
 		VpcId: vpc.VpcId(),
 		Tags: &[]*awscdk.CfnTag{
 			{
 				Key:   jsii.String("name"),
-				Value: jsii.String(routeTableName),
+				Value: jsii.String(defaultRouteTableName),
 			},
 		},
 	})
@@ -70,7 +74,7 @@ func NewCdkIvanStack(scope constructs.Construct, id string, props *CdkStackProps
 	// Adds a route to the internet gateway
 	awsec2.NewCfnRoute(stack, jsii.String("ivan-cdk-route"), &awsec2.CfnRouteProps{
 		RouteTableId:         routeTable.AttrRouteTableId(),
-		DestinationCidrBlock: jsii.String("0.0.0.0/0"),
+		DestinationCidrBlock: jsii.String(defaultDestinationCidrBlock),
 		GatewayId:            vpc.InternetGatewayId(),
 	})
 
@@ -106,11 +110,11 @@ func NewCdkIvanStack(scope constructs.Construct, id string, props *CdkStackProps
 
 	// EKS cluster
 	eksCluster := awseks.NewCluster(stack, jsii.String("eks-cdk-cluster"), &awseks.ClusterProps{
-		Version:         awseks.KubernetesVersion_Of(jsii.String("1.30")),
+		Version:         awseks.KubernetesVersion_Of(jsii.String(defaultK8SVersion)),
 		ClusterName:     jsii.String("eks-cdk-cluster"),
 		Vpc:             vpc,
 		SecurityGroup:   securityGroup,
-		DefaultCapacity: jsii.Number(0),
+		DefaultCapacity: jsii.Number(defaultCapacity),
 		Role:            clusterRole,
 	})
 
@@ -128,23 +132,22 @@ func NewCdkIvanStack(scope constructs.Construct, id string, props *CdkStackProps
 	})
 
 	// Node group
-
 	eksCluster.AddNodegroupCapacity(jsii.String("node-group"), &awseks.NodegroupOptions{
 		InstanceTypes: &[]awsec2.InstanceType{
-			awsec2.NewInstanceType(jsii.String("t2.medium")),
+			awsec2.NewInstanceType(jsii.String(defaultInstanceType)),
 		},
 		NodeRole:    nodeGroupRole,
-		MinSize:     jsii.Number(nodeGroupMinSize),
-		MaxSize:     jsii.Number(nodeGroupMaxSize),
-		DesiredSize: jsii.Number(nodeGroupDesiredSize),
+		MinSize:     jsii.Number(defaultNodeGroupMinSize),
+		MaxSize:     jsii.Number(defaultNodeGroupMaxSize),
+		DesiredSize: jsii.Number(defaultNodeGroupDesiredSize),
 		RemoteAccess: &awseks.NodegroupRemoteAccess{
-			SshKeyName: jsii.String(sshKeyName),
+			SshKeyName: jsii.String(defaultSshKeyName),
 		},
 		Subnets: &awsec2.SubnetSelection{
 			Subnets: vpc.PublicSubnets(),
 		},
 		AmiType:  awseks.NodegroupAmiType_AL2_X86_64,
-		DiskSize: jsii.Number(diskSize),
+		DiskSize: jsii.Number(defaultDiskSize),
 	})
 
 	// Outputs
@@ -163,20 +166,24 @@ func NewCdkIvanStack(scope constructs.Construct, id string, props *CdkStackProps
 func main() {
 	defer jsii.Close()
 
-	flag.StringVar(&vpcCidr, "vpc-cidr", vpcCidr, "CIDR block for the VPC")
-	flag.StringVar(&vpcName, "vpc-name", vpcName, "Name of the VPC")
-	flag.IntVar(&maxAzs, "max-azs", maxAzs, "Maximum Availability Zones")
-	flag.StringVar(&publicSubnetName, "public-subnet-name", publicSubnetName, "Name of the public subnet")
-	flag.StringVar(&privateSubnetName, "private-subnet-name", privateSubnetName, "Name of the private subnet")
-	flag.IntVar(&subnetCidrMask, "subnet-cidr-mask", subnetCidrMask, "Subnet CIDR mask")
-	flag.StringVar(&accountID, "account-id", accountID, "AWS Account ID")
-	flag.StringVar(&region, "region", region, "AWS Region")
-	flag.StringVar(&routeTableName, "route-table-name", routeTableName, "Name of the route table")
-	flag.IntVar(&nodeGroupMinSize, "node-group-min-size", nodeGroupMinSize, "Minimum size of the node group")
-	flag.IntVar(&nodeGroupMaxSize, "node-group-max-size", nodeGroupMaxSize, "Maximum size of the node group")
-	flag.IntVar(&nodeGroupDesiredSize, "node-group-desired-size", nodeGroupDesiredSize, "Desired size of the node group")
-	flag.StringVar(&sshKeyName, "ssh-key-name", sshKeyName, "SSH Key name for EC2 instances")
-	flag.IntVar(&diskSize, "disk-size", diskSize, "Disk size for EC2 instances")
+	flag.StringVar(&defaultVpcCidr, "vpc-cidr", defaultVpcCidr, "CIDR block for the VPC")
+	flag.StringVar(&defaultVpcName, "vpc-name", defaultVpcName, "Name of the VPC")
+	flag.IntVar(&defaultMaxAzs, "max-azs", defaultMaxAzs, "Maximum Availability Zones")
+	flag.StringVar(&defaultPublicSubnetName, "public-subnet-name", defaultPublicSubnetName, "Name of the public subnet")
+	flag.StringVar(&defaultPrivateSubnetName, "private-subnet-name", defaultPrivateSubnetName, "Name of the private subnet")
+	flag.IntVar(&defaultSubnetCidrMask, "subnet-cidr-mask", defaultSubnetCidrMask, "Subnet CIDR mask")
+	flag.StringVar(&defaultAccountID, "account-id", defaultAccountID, "AWS Account ID")
+	flag.StringVar(&defaultRegion, "defaultRegion", defaultRegion, "AWS Region")
+	flag.StringVar(&defaultRouteTableName, "route-table-name", defaultRouteTableName, "Name of the route table")
+	flag.IntVar(&defaultNodeGroupMinSize, "node-group-min-size", defaultNodeGroupMinSize, "Minimum size of the node group")
+	flag.IntVar(&defaultNodeGroupMaxSize, "node-group-max-size", defaultNodeGroupMaxSize, "Maximum size of the node group")
+	flag.IntVar(&defaultNodeGroupDesiredSize, "node-group-desired-size", defaultNodeGroupDesiredSize, "Desired size of the node group")
+	flag.StringVar(&defaultSshKeyName, "ssh-key-name", defaultSshKeyName, "SSH Key name for EC2 instances")
+	flag.StringVar(&defaultDestinationCidrBlock, "destination-cidr-block", defaultDestinationCidrBlock, "The IPv4 CIDR address block used for the destination match")
+	flag.StringVar(&defaultK8SVersion, "default-k8s-version", defaultK8SVersion, "Version of the Kubernetes")
+	flag.StringVar(&defaultInstanceType, "default-instance-type", defaultInstanceType, "Type of instance for the Node")
+	flag.IntVar(&defaultDiskSize, "disk-size", defaultDiskSize, "Disk size for EC2 instances")
+	flag.IntVar(&defaultCapacity, "default-capacity", defaultCapacity, "Number of instances to allocate as an initial capacity for this cluster")
 	flag.Parse()
 
 	app := awscdk.NewApp(nil)
@@ -190,11 +197,11 @@ func main() {
 	app.Synth(nil)
 }
 
-// env determines the AWS environment (account+region) in which our stack is to
+// env determines the AWS environment (account+defaultRegion) in which our stack is to
 // be deployed. For more information see: https://docs.aws.amazon.com/cdk/latest/guide/environments.html
 func env() *awscdk.Environment {
 	return &awscdk.Environment{
-		Account: jsii.String(accountID),
-		Region:  jsii.String(region),
+		Account: jsii.String(defaultAccountID),
+		Region:  jsii.String(defaultRegion),
 	}
 }
